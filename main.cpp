418418cad2dafc2d038da401d69e969653d1ecb2
Clayton <sibandaclayton0@gmail.com>
2018-09-03 14:07:36 +0200

    Feedforward now works

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ public:
 private:
     static double transferFunction(double x);
     static double transferFunctionDerivative(double x);
-    static double randomWeight(void){return rand()/double(RAND_MAX); }
+   // static double randomWeight(void){return rand()/double(RAND_MAX); } not for now
     double m_outputVal;
     vector<double> m_outputWeights;
     unsigned m_myIndex;
@@ -39,17 +39,22 @@ double Neuron::transferFunctionDerivative(double x) {
 
     return (exp(-x))/pow((1+exp(-x)),2);
 }
+
 void Neuron::feedForward(Layer &prevLayer) {
     double sum=0.0;
     //sum the previous layers outputs (which are our inputs)
 
     for(unsigned n=0;n<prevLayer.size();++n){
+        cout<<"output "<<n<<" is "<<prevLayer[n].getOutputVal()<<" index is "<<m_myIndex<<endl;
         sum+=prevLayer[n].getOutputVal()*prevLayer[n].m_outputWeights[m_myIndex];
     }
-cout<<sum<<endl;
+cout<<"sum is "<<sum<<endl;
     m_outputVal = Neuron::transferFunction(sum);
+    cout<<"output is "<<m_outputVal<<endl;
 
 }
+
+
 //helper function used when populating the constructor
 
 void neuronHelper(unsigned layerNum, unsigned m_myIndex,vector<double> &m_outputWeights){
@@ -62,7 +67,9 @@ void neuronHelper(unsigned layerNum, unsigned m_myIndex,vector<double> &m_output
     if(layerNum==0 && m_myIndex==2){
         m_outputWeights= {0.5,-0.6};
     }
-
+    if(layerNum==0 && m_myIndex==3){// bias neuron, every layer has a bia neuron
+        m_outputWeights= {1,1};
+    }//bias neuron
     //for layer2
 
     if(layerNum==1 && m_myIndex==0){
@@ -71,20 +78,23 @@ void neuronHelper(unsigned layerNum, unsigned m_myIndex,vector<double> &m_output
     if(layerNum==1 && m_myIndex==1){
         m_outputWeights= {1.0};
     }
+    if(layerNum==1 && m_myIndex==2){//bias neuron
+        m_outputWeights= {1.0};
+    }
 }
 
 Neuron::Neuron(unsigned numOutputs, unsigned _myIndex, unsigned _layerNum):m_myIndex(_myIndex),layerNum(_layerNum) {
-    for(unsigned c=0;c<numOutputs;++c){
-       cout<<"neuron here\n";
-
-    }
+//    for(unsigned c=0;c<numOutputs;++c){
+//       cout<<"neuron here\n";
+//
+//    }
 
 neuronHelper(_layerNum,_myIndex,m_outputWeights);
-
-    for(unsigned c=0;c<numOutputs;++c){
-        cout<<m_myIndex<<" "<<m_outputWeights[c]<<endl;
-
-    }
+//    cout<<"numOutputs "<< numOutputs<<endl;
+//    for(unsigned c=0;c<numOutputs;++c){
+//        cout<<m_myIndex<<" "<<m_outputWeights[c]<<endl;
+//
+//    }
 
 }
 
@@ -105,7 +115,7 @@ private:
 void Net::feedForward(const vector<double> &inputVals){
     //assert(inputVals.size()==m_layers[0].size()-1);
     //Assign the input values into the input neurons
- cout<<"go here\n";
+ cout<<"net feedfoward\n";
     for(unsigned i=0;i<inputVals.size();++i){
         m_layers[0][i].setOutputVal(inputVals[i]);
     }
@@ -128,12 +138,16 @@ Net::Net(const vector<unsigned> &topology){
     for(unsigned layerNum=0;layerNum<numLayers;++layerNum){
         m_layers.push_back(layer);
         unsigned numOutputs = layerNum==topology.size()-1?0: topology[layerNum+1];
-
+        cout<<"\nLayereNum "<<layerNum<<endl;
         for(unsigned neuronNum=0;neuronNum<=topology[layerNum];++neuronNum){//also includes the bias nueron
 
             m_layers.back().push_back(Neuron(numOutputs,neuronNum,layerNum));
-            cout<<"made a neuron "<<numOutputs<<" "<<neuronNum<<"\n";
+            cout<<"numOutputs "<<numOutputs<<" neuronNum "<<neuronNum<<"\n";
         }
+
+        // Force the bias node's output to 1.0 (it was the last neuron pushed in this layer):
+        m_layers.back().back()//back()
+        .setOutputVal(1.0);
     }
 }
 
